3759103e089d0b93b1b3c7ec1ffe9b312698a29a
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2023-01-23 22:41:10 +0800

    Update Main.java

--- CP020/Java/Main.java
+++ CP020/Java/Main.java
@@ -1,1 +1,16 @@
-fdghf
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
