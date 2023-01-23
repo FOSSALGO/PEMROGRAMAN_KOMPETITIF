e688d06e9b8fb940fb7509ab39e7c91d32bf8dd5
FOSSALGO <60698625+FOSSALGO@users.noreply.github.com>
2023-01-23 11:21:36 +0800

    Create Sigma02.java

---
+++ CP017/Java/Sigma02.java
@@ -0,0 +1,15 @@
+package fosalgo;
+import java.util.Scanner;
+public class Sigma02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int U[] = new int[n];
+        int sum =0;
+        for (int i = 0; i < n; i++) {
+            U[i] = sc.nextInt();
+            sum = sum + U[i];
+        }        
+        System.out.println(sum);
+    }
+}
